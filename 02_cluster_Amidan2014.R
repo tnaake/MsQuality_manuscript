71588182590cac5b60893fa53e81a4bc6dbdfcca
Thomas Naake <thomasnaake@gmx.de>
2023-03-03 11:30:28 +0100

    refactor  02_cluster_Amidan2014.R

--- 02_cluster_Amidan2014.R
+++ 02_cluster_Amidan2014.R
@@ -10,9 +10,7 @@ if (!require("Rhdf5lib", quietly = TRUE))
     BiocManager::install("Rhdf5lib", ask = FALSE)
 if (!require("mzR", quietly = TRUE))
     BiocManager::install("mzR", ask = FALSE, upgrade = "always")
-    #remotes::install_github("sneumann/mzR", ref = "feature/updatePwiz_3_0_21263", force = TRUE)
 if (!require("Spectra", quietly = TRUE))
-	##remotes::install_github("rformassspectrometry/Spectra", ref = "RELEASE_3_15")
     BiocManager::install("Spectra", ask = FALSE)
 
 remotes::install_github("tnaake/MsQuality", upgrade = "always", force = TRUE)
@@ -48,11 +46,10 @@ fls <- fls[!fls %in% c(
 )]
 
 ## create the Spectra object
-##register(SerialParam(1))
-#sps <- Spectra(fls, backend = MsBackendMzR())
+sps <- Spectra(fls, backend = MsBackendMzR())
 
 ## save the Spectra object as RDS file
-#saveRDS(sps, file = "Amidan2014/Amidan2014_sps.RDS")
+saveRDS(sps, file = "Amidan2014/Amidan2014_sps.RDS")
 print("read Spectra object.")
 sps <- readRDS("Amidan2014/Amidan2014_sps.RDS")
 print("finished reading Spectra object.")
